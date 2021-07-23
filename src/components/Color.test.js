8111d9479267b70cad7310a737b9e7d8938e5a0d
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-07-23 16:29:49 -0500

    set up initial tests

--- src/components/Color.test.js
+++ src/components/Color.test.js
@@ -5,15 +5,38 @@ import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import Color from './Color';
 
+const sampleColor = {
+    color: 'green',
+    code: {hex: '#228B22'},
+    id: 1
+}
+
 test("Renders without errors with blank color passed into component", () => {
+    render(<Color color={sampleColor}/>)
 });
   
 test("Renders the color passed into component", () => {
+    render(<Color color={sampleColor}/>)
+    const color = screen.queryAllByTestId('color')
+    expect(color).toBeInTheDocument()
 });
 
 test("Executes handleDelete and toggleEdit property when the 'x' icon is clicked", () => {
+    const handleDelete = jest.fn()
+    const toggleEdit = jest.fn()
+    render (<Color color={sampleColor}/>)
+    const deleteColor = screen.getByTestId('delete')
+    userEvent.click(deleteColor)
+    expect(handleDelete).toBeCalled()
+    expect(toggleEdit).toBeCalled()
 });
 
 test("Executes setEditColor and toggleEdit property when color div is clicked", () => {
-    
+    const setEditColor = jest.fn()
+    const toggleEdit = jest.fn()
+    render (<Color color={sampleColor}/>)
+    const selectedColor = screen.getByTestId('color')
+    userEvent.click(selectedColor)
+    expect(setEditColor).toBeCalled()
+    expect(toggleEdit).toBeCalled()
 });
--- src/components/ColorList.test.js
+++ src/components/ColorList.test.js
@@ -3,12 +3,26 @@ import MutationObserver from 'mutationobserver-shim';
 
 import { render, screen} from "@testing-library/react";
 import ColorList from './ColorList';
+import userEvent from '@testing-library/user-event';
+
+const sampleColor = {
+    color: 'green',
+    code: {hex: '#228B22'},
+    id: 1
+}
 
 test("Renders an empty list of colors without errors", () => {
+    render(<ColorList color={[]}/>)
 });
 
 test("Renders a list of colors without errors", () => {
+    render(<ColorList color = {sampleColor}/>)
 });
 
 test("Renders the EditForm when editing = true and does not render EditForm when editing = false", () => {
+    const activeEdit = jest.fn()
+    render (<ColorList color={sampleColor}/>)
+    const userEditing = screen.queryAllByTestId('color')
+    userEvent.click(userEditing)
+    expect(activeEdit).toBeCalled()
 });
